fde81218cc289cafbf2ec4cf04186b57eea4ba81
Mat Marquis <mat@matmarquis.com>
2024-07-02 15:04:16 -0400

    Left a `console.log` in there while figuring out the test weirdness.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 export default function markdownItCaption(md) {
 	const original = md.renderer.rules.image;
-	md.renderer.rules.image = (tokens, idx, options, env, self) => {
 
+	md.renderer.rules.image = (tokens, idx, options, env, self) => {
 		const [src, alt, caption] = tokens[idx].attrs,
 			id = 'id-' + performance.now().toString().replace(/\./gi, '');
-
-				console.log( "ID:" , id );
 
 		if (caption) {
 			return `<figure><img src="${src[1]}" alt="${alt[1]}" id="${id}"><figcaption aria-describedby="${id}">${caption[1]}</figcaption></figure>`;
