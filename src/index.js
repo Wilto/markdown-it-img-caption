69d1d4fa41cc990f41ef98a6753e9fff415eb336
Mat Marquis <mat@matmarquis.com>
2024-07-02 15:12:57 -0400

    There we go.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default function markdownItCaption(md) {
 			id = 'id-' + performance.now().toString().replace(/\./gi, '');
 
 		if (caption) {
-			return `<figure><img src="${src[1]}" alt="${alt[1]}" id="${id}"><figcaption aria-describedby="${id}">${caption[1]}</figcaption></figure>`;
+			return `<figure><img src="${src[1]}" alt="${alt[1]}" aria-describedby="${id}"><figcaption id="${id}">${caption[1]}</figcaption></figure>`;
 		}
 		return original(tokens, idx, options, env, self);
 	};
